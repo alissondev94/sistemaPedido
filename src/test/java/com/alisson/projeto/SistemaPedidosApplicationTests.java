f0ef1cbbea3b5abadde62695b3497ea744345688
Alisson Paixao <alissonpx25@gmail.com>
2025-06-04 18:21:01 -0300

    Projeto criado

---
+++ src/test/java/com/alisson/projeto/SistemaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisson.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
